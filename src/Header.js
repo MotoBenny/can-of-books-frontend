9124467f72523ccd4ab45c10b99dc6b5d876ae89
James Brooks <brooksjamesk01@gmail.com>
2022-04-01 20:30:35 -0700

    made header complient with trello and make it Auth0 complient

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Navbar, NavItem } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import './Header.css';
+import LogoutButton from './LogoutButton';
+import LoginButton from './LoginButton';
 
 class Header extends React.Component {
   constructor(props) {
@@ -13,21 +15,22 @@ class Header extends React.Component {
 
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>My Favorite Books</Navbar.Brand>
-        <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
-        <NavItem><Link to="/BestBooks" className="nav-link">Best Books</Link></NavItem>
-        {this.props.user
-        ?
-        <>
-          <NavItem><Link to="/Profile" className="nav-link">Profile</Link></NavItem>
-          <NavItem><Link to="/LogoutButton" className="nav-link">Log Out</Link></NavItem>
-        </>
-        :
-        <></>
-        }
-        
-      </Navbar>
+      <>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand>My Favorite Books</Navbar.Brand>
+          <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
+          {
+            this.props.auth0.isAuthenticated
+              ?
+              <>
+                <NavItem><Link to="/BestBooks" className="nav-link">Best Books</Link></NavItem>
+                <NavItem><Link to="/Profile" className="nav-link">Profile</Link></NavItem>
+                <LogoutButton />
+              </>
+              : <LoginButton />
+          }
+        </Navbar>
+      </>
     )
   }
 }
